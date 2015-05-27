0767e683696fa8ee6f51e2dc54a04ba40e351430
AlexKVal <alexkval@gmail.com>
2015-05-27 17:55:03 +0300

    es5 code

--- app/main.js
+++ app/main.js
@@ -1,41 +1,36 @@
-/* eslint react/no-multi-comp: 0 */
-import React from 'react';
+/* eslint-disable */
+var React = require('react');
+var Nav = require( 'react-bootstrap/lib/Nav' );
+var NavItem = require( 'react-bootstrap/lib/NavItem' );
+var Navbar = require( 'react-bootstrap/lib/Navbar' );
+var DropdownButton = require( 'react-bootstrap/lib/DropdownButton' );
+var MenuItem = require( 'react-bootstrap/lib/MenuItem' );
+var CollapsibleNav = require( 'react-bootstrap/lib/CollapsibleNav' );
 
-import {
-  Navbar,
-  Nav,
-  MenuItem,
-  DropdownButton
-} from 'react-bootstrap';
-
-const Auth = React.createClass({
-  render() {
+var App = React.createClass( {
+  render: function() {
     return (
-      <DropdownButton title="Hello" noCaret navItem>
-        <h6>Login with</h6>
-        <MenuItem href="/auth/facebook">
-          Facebook
-        </MenuItem>
-        <MenuItem href="/auth/twitter">
-          Twitter
-        </MenuItem>
-      </DropdownButton>
-    );
-  }
-});
-
-const App = React.createClass({
-  displayName: 'App',
-
-  render() {
-    return (
-      <Navbar inverse>
-        <Nav right>
-          <Auth />
-        </Nav>
+      <Navbar brand={<a href="/">Jiggle.io</a>} inverse staticTop toggleNavKey={0}>
+        <CollapsibleNav eventKey={0}>
+           <Nav navbar>
+              <NavItem eventKey={1} href='#'>Link</NavItem>
+              <NavItem eventKey={2} href='#'>Link <span className="navbar-new">2</span></NavItem>
+           </Nav>
+           <Nav navbar right>
+              <NavItem eventKey={1} href='#'>Link Right</NavItem>
+              <NavItem eventKey={2} href='#'>Link Right</NavItem>
+              <DropdownButton eventKey={3} title='Dropdown'>
+                <MenuItem eventKey='1'>Action</MenuItem>
+                <MenuItem eventKey='2'>Another action</MenuItem>
+                <MenuItem eventKey='3'>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey='4'>Separated link</MenuItem>
+              </DropdownButton>
+           </Nav>
+        </CollapsibleNav>
       </Navbar>
     );
   }
-});
+} );
 
 React.render(<App />, document.getElementById('app'));
